e8441a1869ba1adb726a51de2acc15c217921efd
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2022-04-14 17:24:22 +0200

    correction mot.py

--- mot.py
+++ mot.py
@@ -24,23 +24,40 @@ def existe(mot,lang):
 
 def compare(essai,mot):
     n=len(essai)
-    m=len(mot)
+    couleur,coul=[0]*n,{0:[],1:[],2:[]}
+    lettre_essais,lettre_mot={},{}
+    comp={}
     L=[]
-
     for j in range(n):
-        if essai[j] in mot:
-            a=1
-            if essai[j] ==mot[j]:
-                a=2
+        L.append(donne_couleur(essai,mot,j))
+        coul[L[j]]+=[(essai[j],j)]
+        if essai[j] not in lettre_essais:
+            lettre_essais[essai[j]]=1
         else:
-            a=0
-        L.append(a)
-                        #for i in range(n):
-                            #if essai[i]==mot[i]:
-                                #L.append(2)
-                        # elif essai[i] in mot:
-                        #     L.append(1)
-                        # elif essai[i] not in mot:
-                        #     L.append(0)
+            lettre_essais[essai[j]]+=1
+        if mot[j] not in lettre_mot:
+            lettre_mot[mot[j]]=1
+        else:
+            lettre_mot[mot[j]]+=1
+    for l in coul[2]:
+        L[l[1]]=2
+        if essai[l[1]] not in comp:
+            comp[essai[l[1]]]=1
+        else:
+            comp[essai[l[1]]]+=1
+    for j in range(n):
+        if L[j]!=2:
+            if (essai[j],j) in coul[1]:
+                if essai[j] not in comp and essai[j] in lettre_mot:
+                    couleur[j]=1
+                elif comp[essai[j]]!=lettre_mot[essai[j]]:
+                    couleur[j]=1
     return L
-print(compare('merci','raree'))
+
+def donne_couleur(essai,mot,j):
+    if essai[j]== mot[j]:
+        return 2
+    elif essai[j] in mot[:j]+mot[j+1:]:
+        return 1
+    else:
+        return 0
