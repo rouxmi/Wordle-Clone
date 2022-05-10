8f8bef9d28df40edec5154e965fd17363a5ab850
Lucie <lucie.boucher@telecomnancy.eu>
2022-05-10 11:44:46 +0200

    debut de solveur

---
+++ solveur/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <errno.h>
+#include <assert.h>
+
+void randomInFile(){
+    FILE *f;
+    char c;
+    f=fopen("test.txt","rt");
+    while((c=fgetc(f))!=EOF){
+        printf("%c",c);
+    }
+}
+
+int main(){
+    
+    randomInFile();
+
+    return 0;
+}
