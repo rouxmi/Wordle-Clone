c00e0a0ea038a8ef9f09cddcdd02c3e419d2d841
Mathilde Bard <mathilde.bard@telecomnancy.eu>
2022-05-17 23:04:19 +0200

    une fonction pour le dico de plus

--- solveur/test_dictionnaire.c
+++ solveur/test_dictionnaire.c
@@ -16,10 +16,10 @@ int main()
     printf("la réponse est 5\n");
     char *non="bou";
     char *mot="motus";
-    printf("%d\n",contains(non,mot));
+    printf("%d\n",contains(non,mot,5));
     printf("la réponse est vrai \n");
     char * oui="zyeh";
-    printf("%d\n",contains(oui,mot));
+    printf("%d\n",contains(oui,mot,5));
     printf("la réponse est fausse \n");
 
     printf("%d\n",acetteplace(1,'o',mot));
@@ -27,6 +27,14 @@ int main()
     printf("%d\n",acetteplace(4,'s',mot));
     printf("la réponse est vraie\n");
     printf("%d\n",acetteplace(2,'o',mot));
-    printf("la réponse est faux\n"); //marche
+    printf("la réponse est faux\n"); 
+    char* lucie="abdtjut";
+    printf("%d\n",containsexceptposition(lucie,'t',3,7));
+    printf("la réponse est vraie\n");
+    char* lola="abdtju";
+    printf("%d\n",containsexceptposition(lucie,'t',3,6));
+    printf("la réponse est faux\n");
+    
+
     return 0;
 }
