520a22fafebdef69a36fe6af7b33bf045bb0503b
Montignier <lola.montignier@telecomnancy.eu>
2022-05-17 23:16:09 +0200

    arrangement pour creer des elements tableaux

--- solveur/tableau.c
+++ solveur/tableau.c
@@ -1,21 +1,17 @@
 #include "tableau.h"
 
-void init_tableau(element tab[30],int nbr_essai, int nbr_lettres){
-    int size= nbr_essai * nbr_lettres;
-
+void init_tableau(element tab[30],element L){
     for(int i=0; i<30; i++){
-
-        element L;
-        init_element(&L,'R',1,3,5);
-        tab[i]=L;
-
+        if(sizeof(tab[i])==0){
+            tab[i]=L;
+        }
     }
 }
 
-void  init_element(element *elt,char lettre1 ,int place1, int position1, int essai1){
+void  init_element(element *elt,char lettre1 ,int place1, int coloration1, int essai1){
     elt->lettre =lettre1 ;
     elt->place= place1 ;
-    elt->position =position1;
+    elt->coloration =coloration1;
     elt->essai= essai1 ;
     
 }
@@ -33,16 +29,28 @@ void print_tableau(element tab[30]){
     }
 }
 
+void ajout_mot(char* mot, int num_essai, element tab[30], char* coloration,int taille_mot){
+    //printf("%c",mot[1]);
+    
+    for(int i=0; i<taille_mot;i++){
+        element L;
+        int color;
+        char coloration_i=(int)(coloration[i]);
+        sscanf(&coloration_i,"%d",&color);
+        //printf("%d\n",color);
+        init_element(&L,mot[i],i+1,coloration_i,num_essai);
+        print_element(L);
+        printf("\n");
+        init_tableau(tab,L);
+        //print_tableau(tab);
+        
+    }
+}
+
 int main(){
-    char lettre='R';
-    int place=1;
-    int position =2;
-    int essai= 2;
-    element L;
-    init_element(&L,lettre ,place , position,essai);
     element  T[30] ;
-    init_tableau(T,1,1);
-    print_tableau(T);
+    ajout_mot("merci",1,T,"20202",5);
+    //print_tableau(T);
     return 0;
 }
 
