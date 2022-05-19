eeed3bddde255c0e4769aefe61a1a04e0194ea12
Mathilde Bard <mathilde.bard@telecomnancy.eu>
2022-05-19 13:31:56 +0200

    épuration dico

--- solveur/dictionnaire.h
+++ solveur/dictionnaire.h
@@ -4,15 +4,20 @@
 #include <err.h>
 #include <errno.h>
 #include <assert.h>
+#include "tableau.h"
 
-#ifdef __DICTIONNAIRE__H__
+#ifndef __DICTIONNAIRE__H__
 #define __DICTIONNAIRE__H__
 
-int taillefichiertxt ();
-bool contains(char *pasceslettres , char* mot, int longueur_mot);
-
-bool acetteplace(int position, char x, char mot[]);
-bool containsexceptposition(char* mot,char x,int position, int longueur_mot);
-
+int taillefichiertxt ();//
+bool contains(char *ceslettres , char* mot, int longueur_mot);//
+bool acetteplace(int position, char x, char mot[]);//
+bool containsexceptposition(char* mot,char x,int position, int longueur_mot);//
+char * contentofline(FILE *f, int linenumber, int longueur_mot, int longueur_dico);//
+void str_slice(char * str, char * word_coupe, size_t start, size_t end);//
+void dico(element* tab[30], int nb_essai, int longueur_mot, int longueur_fichier);
+char * add(char *str, char x);//
+char* createplace(int longueur_mot);
+void addintofile(FILE *f, char *word_to_add,int longueur_fichier);
 
 #endif
