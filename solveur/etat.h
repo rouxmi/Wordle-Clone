3ced77bb554bf399799d043e7b343edb78ff5d35
Lucie <lucie.boucher@telecomnancy.eu>
2022-05-19 17:58:41 +0200

    etat.h

--- solveur/etat.h
+++ solveur/etat.h
@@ -17,11 +17,25 @@ typedef struct etat{
     bool terminal;
 } etat;
 
-typedef struct liste_etat{
-    etat actuel;
-    struct liste_etat* suivant;
-} liste_etat;
+typedef struct arbre_etat{
+    etat node;
+    struct arbre_etat* gauche;
+    struct arbre_etat* droit;
 
+} arbre_etat;
+
+void etat_init(etat *e, int id, bool ter);
+void etat_print(etat e);
+
+arbre_etat* arbre_etat_create();
+
+arbre_etat* arbre_etat_composee(etat n, arbre_etat* g, arbre_etat* d);
+
+bool arbre_etat_est_vide(arbre_etat* a);
+
+void arbre_etat_delete (arbre_etat* a);
+
+void arbre_etat_print(arbre_etat* a);
 
 
 #endif //GRAPH_ETAT_H
