4ac3674afb646d4e62e694242fdc663159bb30e5
Montignier <lola.montignier@telecomnancy.eu>
2022-05-27 18:41:31 +0200

    essai rectification interface

--- solveur/interface.c
+++ solveur/interface.c
@@ -7,37 +7,43 @@
 #include <errno.h>
 #include <assert.h>
 #include <string.h>
-/*
+
 int main(){
     int nbr_essais= 5;
     FILE *fichier=fopen("texte/wsolf.txt","r");
-    //int taille_fichier =fread();
-    int longueur_mot = 5;
-    printf("%d \n",longueur_mot);
-    element T[nbr_essais*longueur_mot];
+    char* longueur_mot=contentofline(fichier,0,1,0);
+    //printf("%s \n",longueur_mot);
+    int longueur=strtol(longueur_mot,NULL,10);
+    //printf("%d \n",longueur);
+    
+    element T[nbr_essais*longueur];
     for (int i =0; i < nbr_essais; i++)
     {
-        char* coloration;
+        int coloration;
         char* mot_test= randomInFile("dicointer1.txt");
         printf("le mot à tester est %s \n",mot_test);
         printf("Entrez la coloration \n");
         scanf("%c",coloration);
-        if(coloration=="-1"){
+        if(coloration== -1){
             printf("vous arrêtez de jouer");
             break;
         }
-        if(coloration=="22222"){
+        if(coloration==22222){
             printf("le solveur a gagné");
             break;
         }
         else{
-            ajout_mot(mot_test,0,T,coloration,longueur_mot);
+            char* color;
+            sprintf(color,"%c",coloration);
+            ajout_mot(mot_test,0,T,color,longueur);
+            dico(T,i,longueur);
 
         }
     }
+    
     fclose(fichier);
     //print_tableau(T)
     return 0;
 }
 
-*/
+
